70f955bcf19b387df6711d376f852c3e847f0f1b
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-11-30 20:00:45 -0500

    setting up the arrrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
 
+var passwordMinimumLength = 8;
+var passwordMaximumLength = 128;
+var password =[]
+var specialCharects = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+console.log (specialCharects)
+var numbersList = ['0','1','2','3','4','5','6','7', '8', '9']
+
+var uppersList = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+
+var lowersList = "abcdefghijklmnopqrstuvwxyz";
 function generatePassword() {
-  // YOUR CODE GOES HERE
-  // characters, minimum and maximum length
-  var charetcs = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz0123456789\
-  ()ABCDEFGHIJKLMOPQRSTUVWXYZ";
-  var passwordMinimumLength = 8;
-  var passwordMaximumLength = 128;
-  var password ="";
-  var specialCharects = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-  var numbersList = "0123456789";
-  var uppersList = "ABCDEFGHIJKLMNOPQRSTUVXYZ".toUpperCase();
-  var lowersList = "abcdefghijklmnopqrstuvwxyz";
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?'),
+    10
+  );
+  alert(length)
+  var randomEl = Math.floor(Math.random() * numbersList.length)
+  // alert(randomEl)
+  password.push(randomEl)
+
 }
 
 // acceptable password
-var passwordRules = prompt('Character types Accepted in Password abcdefghijklmnopqrstuvwxyz0123456789\
-()ABCDEFGHIJKLMOPQRSTUVWXYZ ' + 1 + specialCharects);
+// 1 + specialCvar passwordRules = prompt('Character types Accepted in Password abcdefghijklmnopqrstuvwxyz0123456789\
+// ()ABCDEFGHIJKLMOPQRSTUVWXYZ ' + harects);
 
 
 
